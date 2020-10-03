2a9ed08bbb647edb299eeaa0a29ed78e7620a2db
Now Kim <peterhyunjae@naver.com>
2020-10-03 23:38:14 +0900

    devide components, menu,footer,title. and they are in site folder

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -6,7 +6,7 @@
       dark
     >
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>Page title</v-toolbar-title>
+      <site-title :title="title"></site-title>
       <v-spacer></v-spacer>
         <v-btn icon to="/about">
           <v-icon>mdi-magnify</v-icon>
@@ -28,50 +28,27 @@
       </v-list-item>
 
       <v-divider></v-divider>
-
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
+      <site-menu></site-menu>
     </v-navigation-drawer>
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-footer padless app color="primary" dark absolute>
-    <v-col
-      class="text-center"
-      cols="12"
-    >
-      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-    </v-col>
-  </v-footer>
+    <site-footer :footer="footer"></site-footer>
   </v-app>
 </template>
 
 <script>
+import SiteTitle from '@/views/site/title'
+import SiteFooter from '@/views/site/footer'
+import SiteMenu from '@/views/site/menu'
+
 export default {
+  components: { SiteTitle, SiteFooter, SiteMenu },
   name: 'App',
   data () {
     return {
-      items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' }
-      ],
+      title: '나의 타이틀입니다',
+      footer: 'footer입니다',
       right: null,
       drawer: false
     }
---
+++ helloworld/src/views/site/footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card height="150">
+    <v-footer
+      absolute
+      :footer="footer"
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() + ' ' + footer}} — <strong>Vuetify</strong>
+      </v-col>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['footer']
+}
+</script>
