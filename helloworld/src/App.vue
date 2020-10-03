d1e1145ad93f7c558df6220b3a26bbb5304868ec
Now Kim <peterhyunjae@naver.com>
2020-10-03 19:03:11 +0900

    made v-app-bar

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/xxx">xxx</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Page title</v-toolbar-title>
+      <v-spacer></v-spacer>
+        <v-btn icon to="/about">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+        <v-btn icon to="/">
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+    </v-app-bar>
+    <router-view>
+    </router-view>
+  </v-app>
 </template>
 
-<style>
-</style>
+<script>
+export default {
+  name: 'App'
+}
+</script>
